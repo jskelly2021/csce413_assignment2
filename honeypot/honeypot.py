015075aa16c9be6f1d1268989948d73fef0f6a7c
Jacob Kelly <jskelly2021@gmail.com>
2026-02-12 00:59:13 +0000

    Started honeypot

--- honeypot/honeypot.py
+++ honeypot/honeypot.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-"""Starter template for the honeypot assignment."""
 
 import logging
 import os
 import time
+import threading
 
 LOG_PATH = "/app/logs/honeypot.log"
+PORT = "2222"
+
+
+def handle_connection(conn, addr, connect_time):
+    """Handle the connection attempt"""
 
 
 def setup_logging():
@@ -21,6 +26,23 @@ def run_honeypot():
     logger = logging.getLogger("Honeypot")
     logger.info("Honeypot starter template running.")
     logger.info("TODO: Implement protocol simulation, logging, and alerting.")
+
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(("0.0.0.0", PORT))
+            s.listen()
+
+            logging.info("Listening for connection attempts on port %s...", PORT)
+
+            while True:
+                conn, addr = s.accept()
+                connect_time = time.time()
+
+                logging.info("Connection attempt from %s at %s", addr[0], time.ctime(connect_time))
+                threading.Thread(target=handle_connection, args=(conn, addr[0], connect_time), daemon=True).start()
+
+    except Exception as e:
+        logging.error("Error listening on port %s: %s", PORT, e)
 
     while True:
         time.sleep(60)
--- port_knocking/README.md
+++ port_knocking/README.md
@@ -1,8 +1,7 @@
 ## Port Knocking Starter Template
 ### Design
 * Each port in the sequence has a thread dedicated to listening for knocks.
-* When it gets a knock, it updates a registry of IP addresses and previous knocks and
-their times.
+* When it gets a knock, it updates a registry of IP addresses and previous knocks and their times.
 * It then checks:
     * Have there been the correct # of knocks for the sequence. If yes checks:
         * If not, continue listening.
@@ -14,13 +13,13 @@ their times.
 * Implemented with TCP connections
 * Uses iptables to handle networking rules
 
-Starting the server:
-* flushes the rules with iptables -F
-* Disallows Inbound traffic on the protected port
-* Allows traffic on all the sequenced ports
+* Starting the server:
+    * flushes the rules with iptables -F
+    * Disallows Inbound traffic on the protected port
+    * Allows traffic on all the sequenced ports
 
 ### Example Usage
-* WARNING: Only run in a container. Flushes all entries of the iptable when server starts.
+* WARNING: Only run in the server script in a container. Flushes all entries of the iptable when server starts.
 
 ```bash
 python3 knock_client.py --target 172.20.0.40 --sequence 1234,5678,9012
